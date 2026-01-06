dfa40f73bf2d6aa5d86f7abc459a47f5eb0a9ad0
Jamon Bailey <infinity.dev@outlook.com>
2026-01-05 21:51:14 -0500

    Update dev_exe.cpp

--- tests/cli/dev_exe.cpp
+++ tests/cli/dev_exe.cpp
@@ -1,11 +1,40 @@
 
 #include <iostream>
+#include <string>
+
+#include"xfs/path/interface/path_policy.hpp"
+#include"xfs/path/interface/system_path.hpp"
+#include"xfs/platform/win32/path/policy.hpp"
+
+template <typename T>
+using FileSystemPath = xfs::path::FileSystemPath<T>;
+
+template <typename T>
+using WindowsPathPolicy = xfs::win32::path::WindowsPathPolicy<T>;
+
+// Development demonstration
+class DemoPath : public FileSystemPath<WindowsPathPolicy<wchar_t>> {
+public:
+    DemoPath(const StringType& path) noexcept
+        : pathStr{path}
+    {
+        //
+    }
+
+private:
+    StringType pathStr;
+};
 
 int main(int argc, char* argv[])
 {
     //\\//
     //
 
-    std::cout << "\nFunctioning properly." << std::endl;
+    //constexpr const wchar_t* tempDir = L"C:\\Users\\TEMP\\xfs\\..\\xfs_install\\.";
+    const std::wstring tDir = L"C:\\Users\\TEMP\\xfs\\..\\xfs_install\\.";
+    
+    DemoPath demo{tDir};
+    //
+
     return 0;
 }
